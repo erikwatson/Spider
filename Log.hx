a4380c239ceecf2e1720b74887379a783a2a2a60
Erik Watson <championchap@gmail.com>
2015-08-06 00:21:37 +0100

    moved getTime and getUKDateFormat to Utils, turns out we did need it after all

--- Log.hx
+++ Log.hx
@@ -6,6 +6,7 @@ import sys.io.FileOutput;
 
 import spider.Spider;
 import spider.Config;
+import spider.Utils;
 
 // Easily output Error messages, etc to a log file
 
@@ -18,7 +19,7 @@ class Log
 	public static function message(str:String) {
 		var messageOutput:String = "";
 
-		messageOutput += getTime();
+		messageOutput += Utils.getTime();
 		messageOutput += " : ";
 		messageOutput += str;
 		messageOutput += "\n \n";
@@ -32,7 +33,7 @@ class Log
 	public static function warning(str:String) {
 		var messageOutput:String = "";
 
-		messageOutput += getTime();
+		messageOutput += Utils.getTime();
 		messageOutput += " : ";
 		messageOutput += "**Warning : **";
 		messageOutput += str;
@@ -47,7 +48,7 @@ class Log
 	public static function error(str:String) {
 		var messageOutput:String = "";
 
-		messageOutput += getTime();
+		messageOutput += Utils.getTime();
 		messageOutput += " : ";
 		messageOutput += "**Error : **";
 		messageOutput += str;
@@ -67,7 +68,7 @@ class Log
 			var filePath:String = '${Spider.config.logLocation}';
 			var fileExtension:String = ".md";
 
-			filePath += getUKDateFormat();
+			filePath += Utils.getUKDateFormat();
 			filePath += fileExtension;
 
 			var file;
@@ -84,75 +85,6 @@ class Log
 
 			output = "";
 		}
-	}
-
-	private static function getUKDateFormat():String {
-		var output = "";
-
-		var day:Int;
-		var month:Int;
-		var year:Int;
-
-		day = Date.now().getDate();
-		month = Date.now().getMonth() + 1;
-		year = Date.now().getFullYear();
-
-		if(day < 10) {
-			output += '0$day';
-		} else {
-			output += '$day';
-		}
-
-		output += "-";
-
-		if(month < 10) {
-			output += '0$month';
-		} else {
-			output += '$month';
-		}
-
-		output += "-";
-
-		if(year < 10) {
-			output += '0$year';
-		} else {
-			output += '$year';
-		}
-
-		return output;
-	}
-
-	private static function getTime():String {
-		var output = "";
-		var now = Date.now();
-
-		var hours;
-		var minutes;
-		var seconds;
-
-		if(now.getHours() < 10){
-			hours = '0${now.getHours()}';
-		} else {
-			hours = '${now.getHours()}';
-		}
-
-		if(now.getMinutes() < 10){
-			minutes = '0${now.getMinutes()}';
-		} else {
-			minutes = '${now.getMinutes()}';
-		}
-
-		if(now.getSeconds() < 10){
-			seconds = '0${now.getSeconds()}';
-		} else {
-			seconds = '${now.getSeconds()}';
-		}
-
-
-
-		output = '$hours:$minutes:$seconds';
-
-		return output;
 	}
 
 	/*
---
+++ Utils.hx
@@ -0,0 +1,72 @@
+package spider;
+
+class Utils {
+	public static function getUKDateFormat():String {
+		var output = "";
+
+		var day:Int;
+		var month:Int;
+		var year:Int;
+
+		day = Date.now().getDate();
+		month = Date.now().getMonth() + 1;
+		year = Date.now().getFullYear();
+
+		if(day < 10) {
+			output += '0$day';
+		} else {
+			output += '$day';
+		}
+
+		output += "-";
+
+		if(month < 10) {
+			output += '0$month';
+		} else {
+			output += '$month';
+		}
+
+		output += "-";
+
+		if(year < 10) {
+			output += '0$year';
+		} else {
+			output += '$year';
+		}
+
+		return output;
+	}
+
+	public static function getTime():String {
+		var output = "";
+		var now = Date.now();
+
+		var hours;
+		var minutes;
+		var seconds;
+
+		if(now.getHours() < 10){
+			hours = '0${now.getHours()}';
+		} else {
+			hours = '${now.getHours()}';
+		}
+
+		if(now.getMinutes() < 10){
+			minutes = '0${now.getMinutes()}';
+		} else {
+			minutes = '${now.getMinutes()}';
+		}
+
+		if(now.getSeconds() < 10){
+			seconds = '0${now.getSeconds()}';
+		} else {
+			seconds = '${now.getSeconds()}';
+		}
+
+
+
+		output = '$hours:$minutes:$seconds';
+
+		return output;
+	}
+}
