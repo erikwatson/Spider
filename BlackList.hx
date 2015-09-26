af93da455400ac0c11c99354e0c78842ac1a627a
Erik Watson <championchap@gmail.com>
2015-09-27 00:45:16 +0100

    file read stubs

--- BlackList.hx
+++ BlackList.hx
@@ -5,6 +5,7 @@ package spider;
 class BlackList {
 
     public var location(get, null):String;
+    private var contents:Array<String> = [];
 
     public function new() {  }
 
@@ -18,17 +19,41 @@ class BlackList {
     public function anyBlocked() {  }
 
     // block this IP Address
-    public function block() {  }
+    public function blockThis() {
+        readFile();
+    }
+
+    // block this IP Address
+    public function blockThese(addresses:Array<String>) {
+        readFile();
+    }
 
     // unblock this ip address
-    public function unblock() {  }
+    public function unblockThis() {
+        readFile();
+    }
+
+    public function unblockThese(addresses:Array<String>) {
+        readfile();
+    }
+
+    // populate the contents of BlackList
+    // for now, we will just call this every time we modify the block list
+    // to make sure we're up to date, dumb but effective
+    private function readFile() {
+        contents = []; // clear the contents first
+        // does the blacklist exist?
+        // if not, create it
+        // open it now that we are sure it exists
+        // loop through every line of the file and add each line to the contents array
+    }
 
     /*
 
         Getters and Setters
 
     */
-    
+
     private function get_location():String { return Spider.config.blacklistLocation; }
 
 }
